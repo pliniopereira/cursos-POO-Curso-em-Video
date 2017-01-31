4e263a6f3f63c91e5d17827f65f9b7bf9a49c445
Plínio Pereira <pliniojr@gmail.com>
2017-01-31 18:24:03 -0200

    aula04 :punch: :white_check_mark:

---
+++ aula04/src/Caneta.java
@@ -0,0 +1,26 @@
+public class Caneta {
+    public String modelo;
+    private float ponta;
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+
+    public float getPonta(){
+        return this.ponta;
+    }
+
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+
+    public void status() {
+        System.out.println("SOBRE A CANETA");
+        System.out.println("Modelo: " + getModelo());
+        System.out.println("Ponta: " + getPonta());
+    }
+}
