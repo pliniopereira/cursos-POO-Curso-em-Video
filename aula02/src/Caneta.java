f39508d8086fa0bfc2a5af0fcf6cb787fe60b0e4
Plínio Pereira <pliniojr@gmail.com>
2017-01-31 16:06:33 -0200

    Add modules aula02 e aula03

---
+++ aula02/src/Caneta.java
@@ -0,0 +1,24 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.print("Uma caneta " + this.cor);
+        System.out.println(" está tampada? \n" + this.tampada);
+    }
+
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar(){
+
+    }
+}
